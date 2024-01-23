d5c46a073b7fb7658288fac8c092845df8b69962
Youngmin Kim <kimyoungmin@gmail.com>
2024-01-23 19:20:29 +0900

    struct, Debug trait, ...

---
+++ rectangle/src/method.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        length: 50,
+        width: 30,
+    };
+
+    println!("rect1 is {:?}", rect1);
+    println!("rect1 is {:#?}", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
