8f574eaee8003002132056a455a1abc579d187f9
Youngmin Kim <kimyoungmin@gmail.com>
2024-01-29 18:18:32 +0900

    simple package: 1 bin crate, 1 lib crate

---
+++ test_package/myapp/src/main.rs
@@ -0,0 +1,10 @@
+use intutils::addition::add;
+use intutils::subtraction::sub;
+
+fn main() {
+    let added = add(1, 2);
+    let subtracted = sub(1, 2);
+
+    println!("Added: {}", added);
+    println!("Subtracted: {}", subtracted);
+}
