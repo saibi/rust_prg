2d9ac8442ec90b5f8e3f2ed16685d27612f7eab8
Youngmin Kim <kimyoungmin@gmail.com>
2024-02-13 08:27:22 +0900

    how to write tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -40,6 +40,11 @@ impl Guess {
     }
 }
 
+pub fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -96,6 +101,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn another() {
         panic!("Make this test fail");
     }
@@ -113,5 +119,32 @@ mod tests {
         } else {
             Err("Value was not 20".to_string())
         }
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
     }
 }
