4a5d2144198293ed1ccb2d1dcb232b6dfd8a7d46
Youngmin Kim <kimyoungmin@gmail.com>
2024-02-13 10:34:27 +0900

    minigrep: extract logic from main()

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let str = String::from("Hello, world!");
+
+    println!("call again {}", str);
+    hello(str);
+}
+
+fn hello(str: String) {
+    println!("Hello {}", str);
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,31 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // dbg!(args);
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+    Ok(())
 }
 
 struct Config {
@@ -21,17 +34,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            eprintln!(
-                "Not enough arguments.\nusage: {} [ptn] [/path/to/file]",
-                &args[0]
-            );
-            std::process::exit(1);
+            return Err("Not enough arguments.");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
