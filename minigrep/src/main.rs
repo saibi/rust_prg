5621d42cc22fabe6ffb5525db29a96ab06a610f6
Youngmin Kim <kimyoungmin@gmail.com>
2024-02-13 10:10:13 +0900

    minigrep: config.new()

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,15 +5,7 @@ fn main() {
 
     // dbg!(args);
 
-    if args.len() < 3 {
-        eprintln!(
-            "Not enough arguments.\nusage: {} [ptn] [/path/to/file]",
-            &args[0]
-        );
-        std::process::exit(1);
-    }
-
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -28,9 +20,18 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            eprintln!(
+                "Not enough arguments.\nusage: {} [ptn] [/path/to/file]",
+                &args[0]
+            );
+            std::process::exit(1);
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
