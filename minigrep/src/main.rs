f0d8687aa3417c2624da2e18e3b24aa5f54987dd
Youngmin Kim <kimyoungmin@gmail.com>
2024-02-13 10:03:21 +0900

    minigrep: parse_config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -12,13 +12,25 @@ fn main() {
         );
         std::process::exit(1);
     }
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
