d998e026f57cda2cccd1b9af512d27aca73168ae
Youngmin Kim <kimyoungmin@gmail.com>
2024-03-25 19:53:37 +0900

    if, loop, while, for, range

---
+++ flow_control/src/main.rs
@@ -0,0 +1,141 @@
+fn if_else_test() {
+    let n = 5;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    let bin_n = if n < 10 && n > -10 {
+        println!(", and it is a small number, increase ten-fold");
+        10 * n
+    } else {
+        println!(", and it is a big number, reduce by two");
+        n / 2
+    };
+
+    println!("{} -> {}", n, bin_n);
+}
+
+fn loop_test() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            break;
+        }
+    }
+
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+    println!("Exited the outer loop");
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+}
+
+fn fizz_buzz() {
+    let mut n = 1;
+
+    while n < 101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        n += 1;
+    }
+
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+}
+
+fn for_iter() {
+    // iter - iterate over elements without changing them
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    println!("names: {:?}", names);
+
+    // into_iter - consume the collection and return owned values
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    // println!("names: {:?}", names);
+
+    // iter_mut - iterate over mutable references
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+    println!("names: {:?}", names);
+}
+
+fn main() {
+    if_else_test();
+    loop_test();
+    fizz_buzz();
+
+    for_iter();
+}
