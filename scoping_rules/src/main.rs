e70d4d598e5bb782e552743556520cc4386be01d
Youngmin Kim <kimyoungmin@gmail.com>
2024-10-24 15:43:28 +0900

    rust by example: 15.4 lifetimes

--- scoping_rules/src/main.rs
+++ scoping_rules/src/main.rs
@@ -11,6 +11,9 @@ fn main() {
     mutability();
     partial_moves();
     borrowing();
+    test1_main();
+    test2_main();
+    test3_main();
 }
 
 fn mutability() {
@@ -72,4 +75,81 @@ fn borrowing() {
     }
 
     eat_box_i32(boxed_i32);
+}
+
+// A type `Borrowed` which houses a reference to an
+// `i32`. The reference to `i32` must outlive `Borrowed`.
+#[derive(Debug)]
+struct Borrowed<'a>(&'a i32);
+
+// Similarly, both references here must outlive this structure.
+#[derive(Debug)]
+struct NamedBorrowed<'a> {
+    x: &'a i32,
+    y: &'a i32,
+}
+
+// An enum which is either an `i32` or a reference to one.
+#[derive(Debug)]
+enum Either<'a> {
+    Num(i32),
+    Ref(&'a i32),
+}
+
+fn test1_main() {
+    let x = 18;
+    let y = 15;
+
+    let single = Borrowed(&x);
+    let double = NamedBorrowed { x: &x, y: &y };
+    let reference = Either::Ref(&x);
+    let number = Either::Num(y);
+
+    println!("x is borrowed in {:?}", single);
+    println!("x and y are borrowed in {:?}", double);
+    println!("x is borrowed in {:?}", reference);
+    println!("y is *not* borrowed in {:?}", number);
+}
+
+#[derive(Debug)]
+struct Borrowed2<'a> {
+    x: &'a i32,
+}
+
+impl<'a> Default for Borrowed2<'a> {
+    fn default() -> Self {
+        Self { x: &10 }
+    }
+}
+
+fn test2_main() {
+    let b: Borrowed2 = Default::default();
+    println!("b is {:?}", b);
+}
+
+use std::fmt::Debug;
+
+#[derive(Debug)]
+struct Ref<'a, T: 'a>(&'a T);
+
+fn print<T>(t: T)
+where
+    T: Debug,
+{
+    println!("'print': t is {:?}", t);
+}
+
+fn print_ref<'a, T>(t: &'a T)
+where
+    T: Debug + 'a,
+{
+    println!("'print_ref': t is {:?}", t);
+}
+
+fn test3_main() {
+    let x = 7;
+    let ref_x = Ref(&x);
+
+    print_ref(&ref_x);
+    print(ref_x);
 }
