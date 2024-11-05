0ba723b43188ada1eba4d9d0282fba2345d2158b
Youngmin Kim <kimyoungmin@gmail.com>
2024-11-05 18:55:23 +0900

    rust by example: iterating over results

---
+++ boxing_error/src/main.rs
@@ -0,0 +1,79 @@
+use std::error;
+use std::fmt;
+
+// Change the alias to use `Box<dyn error::Error>`.
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+#[derive(Debug, Clone)]
+struct EmptyVec;
+
+impl fmt::Display for EmptyVec {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid first item to double")
+    }
+}
+
+impl error::Error for EmptyVec {}
+
+// fn double_first(vec: Vec<&str>) -> Result<i32> {
+//     vec.first()
+//         .ok_or_else(|| EmptyVec.into()) // Converts to Box
+//         .and_then(|s| {
+//             s.parse::<i32>()
+//                 .map_err(|e| e.into()) // Converts to Box
+//                 .map(|i| 2 * i)
+//         })
+// }
+fn double_first(vec: Vec<&str>) -> Result<i32> {
+    let first = vec.first().ok_or(EmptyVec)?;
+    let parsed = first.parse::<i32>()?;
+    Ok(parsed * 2)
+}
+
+fn print(result: Result<i32>) {
+    match result {
+        Ok(n) => println!("The first doubled is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    let numbers = vec!["42", "93", "18"];
+    let empty = vec![];
+    let strings = vec!["tofu", "93", "18"];
+
+    print(double_first(numbers));
+    print(double_first(empty));
+    print(double_first(strings));
+
+    iter_map();
+    filter_map();
+    map_err();
+}
+
+fn iter_map() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
+    println!("Results: {:?}", numbers);
+}
+
+fn filter_map() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings
+        .into_iter()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+    println!("Results: {:?}", numbers);
+}
+
+fn map_err() {
+    let strings = vec!["tofu", "93", "18", "257"];
+    let mut errors = vec![];
+    let numbers: Vec<_> = strings
+        .into_iter()
+        .map(|s| s.parse::<u8>())
+        .filter_map(|r| r.map_err(|e| errors.push(e)).ok())
+        .collect();
+    println!("Numbers: {:?}", numbers);
+    println!("errors: {:?}", errors);
+}
