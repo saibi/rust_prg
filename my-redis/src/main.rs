790c8a1cf481ca1752ec0072c105850eafafa240
Youngmin Kim <kimyoungmin@gmail.com>
2024-12-02 15:21:26 +0900

    tokio tutorial: spawning

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,15 +1,26 @@
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
-    let result = client.get("hello").await?;
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    println!("got value from the server; result={:?}", result);
-    Ok(())
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
 }
 
-// need mini-redis server
-// $ cargo install mini-redis
-// $ mini-redis-server
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
+}
